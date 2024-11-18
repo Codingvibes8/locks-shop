82ef28793a88ccba942075f312b8f58f6d331c21
Codingvibes8 <wecodeappz@gmail.com>
2024-11-18 12:36:06 +0000

    refactored code to include neondb/postgres/drizzle functions

--- lib/actions/productActions.ts
+++ lib/actions/productActions.ts
@@ -12,13 +12,3 @@ export async function getLatestProducts() {
   return data;
 }
 
-// "use server";
-// app/actions.ts
-
-// import { neon } from "@neondatabase/serverless";
-
-// export async function getData() {
-//     const sql = neon(process.env.DATABASE_URL!);
-//     const data = await sql`...`;
-//     return data;
-// }
